bc25369f418e7782794936c2c72a6b0aa1a0f7f1
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-05-28 01:10:46 +0200

    feat: add eth in defi module

---
+++ src/eth_in_defi.rs
@@ -0,0 +1,92 @@
+use crate::{
+    defi_llama::{self, Protocol},
+    progress,
+};
+use sqlx::PgPool;
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct EthInDefiAtDate {
+    timestamp: chrono::DateTime<chrono::Utc>,
+    eth: f64,
+}
+
+async fn store_eth_in_defi(
+    pool: &PgPool,
+    eth: &f64,
+) -> sqlx::Result<sqlx::postgres::PgQueryResult> {
+    sqlx::query_as!(
+        EthInDefiAtDate,
+        "INSERT INTO eth_in_defi (timestamp, eth) VALUES ($1, $2)",
+        chrono::Utc::now(),
+        eth
+    )
+    .execute(pool)
+    .await
+}
+
+fn print_eth_like_in_protocol(protocol: &Protocol) -> Option<()> {
+    let last_tokens_at_date = protocol
+        .chain_tvls
+        .ethereum
+        .as_ref()
+        .and_then(|chain_tvl| chain_tvl.tokens.last())?;
+
+    for key in last_tokens_at_date.tokens.keys() {
+        if key.contains("ETH") {
+            log::info!(
+                "protocol {} - {}, contains a token that may be ETH, named: {}",
+                protocol.name,
+                protocol.id,
+                key
+            );
+        };
+    }
+
+    Some(())
+}
+
+pub async fn update(pool: &PgPool) {
+    let protocols = defi_llama::get_protocols();
+
+    log::info!("{} protocols to crawl", protocols.len());
+
+    let mut progress =
+        progress::Progress::new(protocols.len().try_into().unwrap(), "crawl protocols", 10);
+
+    let eth_in_protocols = protocols
+        .iter()
+        .map(|protocol| defi_llama::get_protocol(&protocol.slug))
+        .fold(0f64, |sum, protocol| {
+            let eth_in_protocol = get_eth_in_protocol(&protocol);
+
+            print_eth_like_in_protocol(&protocol);
+
+            match eth_in_protocol {
+                None => log::debug!("no wETH in {} - {}", protocol.name, protocol.id),
+                Some(weth) => log::debug!("{} wETH in {} - {}", weth, protocol.name, protocol.id),
+            };
+
+            progress.step();
+
+            sum + eth_in_protocol.unwrap_or(&0f64)
+        });
+
+    store_eth_in_defi(pool, &eth_in_protocols).await.unwrap();
+
+    log::info!(
+        "counted {} wETH total in all protocols on Defi Llama",
+        eth_in_protocols
+    );
+}
+
+fn get_eth_in_protocol(protocol: &defi_llama::Protocol) -> Option<&f64> {
+    protocol
+        .chain_tvls
+        .ethereum
+        .as_ref()?
+        .tokens
+        .last()?
+        .tokens
+        .get("WETH")
+}
