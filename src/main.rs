0bcb38d62ceb892bc6b6665b457fe20313a82533
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-05-31 10:51:27 +0200

    refactor: reword run main error

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::process;
 
 fn main() {
-    println!("this project has no main binary, run one from src/bin");
+    println!("this project has no main binary, run one of the targets");
     process::exit(1)
 }
