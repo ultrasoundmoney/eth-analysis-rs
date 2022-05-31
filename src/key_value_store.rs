b559fad1ccde4a4aae3ca10a7611186067e5a073
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-05-31 10:37:45 +0200

    feat: panic on unrecoverable

--- src/key_value_store.rs
+++ src/key_value_store.rs
@@ -1,7 +1,7 @@
 use serde::Serialize;
 use sqlx::{types::Json, Encode, PgPool, Postgres, Type};
 
-pub async fn store_value<'a, A>(pool: &PgPool, key: &'a str, value: A) -> sqlx::Result<()>
+pub async fn store_value<'a, A>(pool: &PgPool, key: &'a str, value: A)
 where
     A: 'a + Send + Encode<'a, Postgres> + Type<Postgres> + Serialize,
 {
@@ -14,5 +14,5 @@ where
     .bind(Json(value))
     .execute(pool)
     .await
-    .map(|_| ())
+    .unwrap();
 }
