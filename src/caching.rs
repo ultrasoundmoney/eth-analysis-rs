cbd2e47e07563d158a126c0797989f67c8352b4a
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-07-09 20:14:32 +0200

    feat: log cache publishing

--- src/caching.rs
+++ src/caching.rs
@@ -1,6 +1,8 @@
 use sqlx::PgExecutor;
 
 pub async fn publish_cache_update<'a>(pool: impl PgExecutor<'a>, key: &str) {
+    tracing::debug!("publishing cache update: {}", key);
+
     sqlx::query!(
         r#"
             SELECT pg_notify('cache-update', $1)
