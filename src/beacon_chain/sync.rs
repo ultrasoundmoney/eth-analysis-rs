c9c4a63e76751ce59f915bf8e139fdc8b643162f
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-07-10 18:25:19 +0200

    feat: sync beacon states continuously

--- src/beacon_chain/sync.rs
+++ src/beacon_chain/sync.rs
@@ -1,12 +1,16 @@
-use futures::SinkExt;
-use futures::{channel::mpsc, StreamExt};
+use std::cmp::Ordering;
+
+use futures::{channel::mpsc, SinkExt, Stream, StreamExt};
 use serde::Deserialize;
 use sqlx::postgres::PgPoolOptions;
-use sqlx::PgPool;
+use sqlx::{PgExecutor, PgPool};
 use thiserror::Error;
 
+use crate::beacon_chain::{self, beacon_time};
 use crate::{config, decoders::from_u32_string, eth_units::GweiAmount};
 
+use super::node::BeaconBlock;
+use super::BeaconNode;
 use super::{
     balances,
     beacon_time::FirstOfDaySlot,
@@ -16,8 +20,8 @@ use super::{
 };
 
 pub struct SlotRange {
-    pub greater_than_or_equal_to: u32,
-    pub less_than: u32,
+    pub greater_than_or_equal: u32,
+    pub less_than_or_equal: u32,
 }
 
 async fn store_state_with_block(
@@ -63,17 +67,13 @@ pub enum SyncError {
     SqlxError(#[from] sqlx::Error),
 }
 
-async fn sync_slot(
-    pool: &PgPool,
-    node_client: &reqwest::Client,
-    slot: &u32,
-) -> Result<(), SyncError> {
-    let state_root = node::get_state_root_by_slot(node_client, slot).await?;
+async fn sync_slot(pool: &PgPool, beacon_node: &BeaconNode, slot: &u32) -> Result<(), SyncError> {
+    let state_root = beacon_node.get_state_root_by_slot(slot).await?;
 
-    let header = node::get_header_by_slot(node_client, slot).await?;
+    let header = beacon_node.get_header_by_slot(slot).await?;
     let block = match header {
         None => None,
-        Some(ref header) => Some(node::get_block_by_root(node_client, &header.root).await?),
+        Some(ref header) => Some(beacon_node.get_block_by_root(&header.root).await?),
     };
     let deposit_sum_aggregated = match block {
         None => None,
@@ -108,7 +108,7 @@ async fn sync_slot(
     };
 
     if let Some(start_of_day_date_time) = FirstOfDaySlot::new(slot) {
-        let validator_balances = node::get_validator_balances(node_client, &state_root).await?;
+        let validator_balances = beacon_node.get_validator_balances(&state_root).await?;
         let validator_balances_sum_gwei = balances::sum_validator_balances(validator_balances);
         balances::store_validator_sum_for_day(
             pool,
@@ -132,25 +132,21 @@ async fn sync_slot(
     Ok(())
 }
 
-async fn sync_slots(
-    pool: &PgPool,
-    node_client: &reqwest::Client,
-    SlotRange {
-        greater_than_or_equal_to: from,
-        less_than: to,
-    }: SlotRange,
-) {
+async fn sync_slots(pool: &PgPool, beacon_node: &BeaconNode, slot_range: &SlotRange) {
     tracing::info!(
-        "syncing slots from {}, to {}, {} slots total",
-        from,
-        to,
-        to - from
+        "syncing slots gte {}, and lte {}, {} slots total",
+        slot_range.greater_than_or_equal,
+        slot_range.less_than_or_equal,
+        slot_range.less_than_or_equal - slot_range.greater_than_or_equal + 1
     );
 
-    let mut progress = pit_wall::Progress::new("sync slots", (to - from).into());
+    let mut progress = pit_wall::Progress::new(
+        "sync slots",
+        (slot_range.less_than_or_equal - slot_range.greater_than_or_equal + 1).into(),
+    );
 
-    for slot in from..=to {
-        sync_slot(pool, node_client, &slot).await.unwrap();
+    for slot in slot_range.greater_than_or_equal..=slot_range.less_than_or_equal {
+        sync_slot(pool, beacon_node, &slot).await.unwrap();
 
         progress.inc_work_done();
         if progress.work_done != 0 && progress.work_done % 1000 == 0 {
@@ -160,14 +156,14 @@ async fn sync_slots(
 }
 
 #[derive(Debug, Deserialize)]
-struct FinalizedCheckpoint {
+struct FinalizedCheckpointEvent {
     block: String,
     state: String,
     #[serde(deserialize_with = "from_u32_string")]
     epoch: u32,
 }
 
-fn stream_finalized_checkpoints() -> mpsc::UnboundedReceiver<FinalizedCheckpoint> {
+fn stream_finalized_checkpoints() -> mpsc::UnboundedReceiver<FinalizedCheckpointEvent> {
     let (mut finalized_checkpoint_tx, finalized_checkpoint_rx) = mpsc::unbounded();
 
     let url_string = format!(
@@ -184,7 +180,7 @@ fn stream_finalized_checkpoints() -> mpsc::UnboundedReceiver<FinalizedCheckpoint
             match event.event_type {
                 Some(ref event_type) if event_type == "finalized_checkpoint" => {
                     let finalized_checkpoint =
-                        serde_json::from_str::<FinalizedCheckpoint>(&event.data).unwrap();
+                        serde_json::from_str::<FinalizedCheckpointEvent>(&event.data).unwrap();
 
                     tracing::debug!(
                         "received new finalized checkpoint {:#?}",
@@ -212,6 +208,47 @@ fn stream_finalized_checkpoints() -> mpsc::UnboundedReceiver<FinalizedCheckpoint
     finalized_checkpoint_rx
 }
 
+async fn get_range_to_sync<'a>(
+    executor: impl PgExecutor<'a>,
+    last_finalized_block_on_start: &BeaconBlock,
+) -> Option<SlotRange> {
+    let last_synced_state = states::get_last_state(executor).await;
+
+    tracing::debug!(
+        "last synced state: {}",
+        last_synced_state
+            .as_ref()
+            .map_or("?".to_string(), |state| state.slot.to_string())
+    );
+
+    match last_synced_state {
+        Some(last_synced_state) => {
+            let last_synced_cmp_last_finalized = last_synced_state
+                .slot
+                .cmp(&last_finalized_block_on_start.slot);
+
+            match last_synced_cmp_last_finalized {
+                Ordering::Less => Some(SlotRange {
+                    greater_than_or_equal: last_synced_state.slot + 1,
+                    less_than_or_equal: last_finalized_block_on_start.slot + 1,
+                }),
+                Ordering::Equal => None,
+                Ordering::Greater => {
+                    panic!(
+                        "last synced is ahead of on-chain last finalized, rollback not implemented"
+                    );
+                }
+            }
+        }
+        None => Some(SlotRange {
+            greater_than_or_equal: 0,
+            less_than_or_equal: last_finalized_block_on_start.slot,
+        }),
+    }
+}
+
+const SLOTS_PER_EPOCH: u32 = 32;
+
 pub async fn sync_beacon_states() {
     tracing_subscriber::fmt::init();
 
@@ -225,54 +262,59 @@ pub async fn sync_beacon_states() {
 
     sqlx::migrate!().run(&pool).await.unwrap();
 
-    let node_client = reqwest::Client::new();
+    let beacon_node = BeaconNode::new();
 
-    let latest_finalized_block_on_start = super::get_last_finalized_block(&node_client)
+    // If we're synced with the latest on-chain checkpoint, we can start listening for and syncing
+    // to new checkpoints as they come in. If we're not synced, we'd like to start syncing right
+    // away, to whatever the current last finalized checkpoint is, whilst concurrently listening
+    // for and queueing new finalized checkpoints.
+
+    // Get the block we need to be synced to before we can listen and process new finalized
+    // checkpoints.
+    let last_finalized_block_on_start = beacon_node
+        .get_last_finalized_block()
         .await
         .expect("last finalized block to be available");
 
     tracing::debug!(
         "last finalized block on start: {}",
-        latest_finalized_block_on_start.slot
+        last_finalized_block_on_start.slot
     );
 
-    let last_synced_state = states::get_last_state(&pool).await;
-
-    tracing::debug!(
-        "last synced state: {}",
-        last_synced_state
-            .as_ref()
-            .map_or("?".to_string(), |state| state.slot.to_string())
-    );
-
+    // Start listening for new finalized checkpoints.
     let mut finalized_checkpoint_stream = stream_finalized_checkpoints();
 
-    match last_synced_state {
-        Some(last_synced_state) => {
-            sync_slots(
-                &pool,
-                &node_client,
-                SlotRange {
-                    greater_than_or_equal_to: last_synced_state.slot as u32 + 1,
-                    less_than: latest_finalized_block_on_start.slot,
-                },
-            )
-            .await;
+    // Sync to last finalized block on start.
+    let range_to_sync_on_start = get_range_to_sync(&pool, &last_finalized_block_on_start).await;
+    match range_to_sync_on_start {
+        Some(slot_range) => {
+            tracing::debug!("slots to sync on-start, syncing");
+            sync_slots(&pool, &beacon_node, &slot_range).await;
         }
         None => {
-            sync_slots(
-                &pool,
-                &node_client,
-                SlotRange {
-                    greater_than_or_equal_to: 0,
-                    less_than: latest_finalized_block_on_start.slot,
-                },
-            )
-            .await;
+            tracing::debug!("no slots to sync on-start, skipping sync");
         }
     }
 
+    // Process queue of new finalized checkpoints.
     while let Some(checkpoint) = finalized_checkpoint_stream.next().await {
-        dbg!(checkpoint);
+        tracing::debug!(
+            "processing next finalized checkpoint, stream lower bound: {}",
+            &finalized_checkpoint_stream.size_hint().0
+        );
+
+        let last_synced_state = states::get_last_state(&pool)
+            .await
+            .expect("synced slots after sync on-start");
+
+        sync_slots(
+            &pool,
+            &beacon_node,
+            &SlotRange {
+                greater_than_or_equal: last_synced_state.slot + 1,
+                less_than_or_equal: (checkpoint.epoch * SLOTS_PER_EPOCH),
+            },
+        )
+        .await;
     }
 }
