71d041a8da9aca1fb677e49fc7bc54f9fc2df8d9
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-07-12 16:18:42 +0200

    feat: get a deposits sum

--- src/beacon_chain/deposits.rs
+++ src/beacon_chain/deposits.rs
@@ -1,8 +1,12 @@
-use sqlx::PgPool;
+use serde::{Deserialize, Serialize};
+use sqlx::postgres::PgRow;
+use sqlx::{PgExecutor, Row};
 
 use crate::eth_units::GweiAmount;
 
-use super::{blocks, node::BeaconBlock};
+use super::blocks::get_deposit_sum_from_block_root;
+use super::node::BeaconBlock;
+use super::states::Slot;
 
 // Obtained by getting validator balances for slot 0, which had zero deposits.
 pub const INITIAL_DEPOSITS: GweiAmount = GweiAmount(674144000000000);
@@ -15,17 +19,105 @@ pub fn get_deposit_sum_from_block(block: &BeaconBlock) -> GweiAmount {
         .fold(GweiAmount(0), |sum, deposit| sum + deposit.data.amount)
 }
 
-pub async fn get_deposit_sum_aggregated(
-    pool: &PgPool,
+pub async fn get_deposit_sum_aggregated<'a>(
+    executor: impl PgExecutor<'a>,
     block: &BeaconBlock,
 ) -> sqlx::Result<GweiAmount> {
     let parent_deposit_sum_aggregated = if block.slot == 0 {
         GweiAmount(0)
     } else {
-        blocks::get_deposit_sum_from_block_root(pool, &block.parent_root).await?
+        get_deposit_sum_from_block_root(executor, &block.parent_root).await?
     };
 
     let deposit_sum_aggregated = parent_deposit_sum_aggregated + get_deposit_sum_from_block(&block);
 
     Ok(deposit_sum_aggregated)
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+pub struct BeaconDepositsSum {
+    deposits_sum: GweiAmount,
+    slot: Slot,
+}
+
+pub async fn get_deposits_sum<'a>(executor: impl PgExecutor<'a>) -> BeaconDepositsSum {
+    sqlx::query(
+        "
+            SELECT slot, deposit_sum_aggregated FROM beacon_states
+            JOIN beacon_blocks ON beacon_blocks.state_root = beacon_states.state_root
+            ORDER BY beacon_states.slot DESC
+            LIMIT 1
+        ",
+    )
+    .map(|row: PgRow| {
+        let slot = row.get::<i32, _>("slot") as u32;
+        let deposits_sum = row.get::<i64, _>("deposit_sum_aggregated").into();
+        BeaconDepositsSum { deposits_sum, slot }
+    })
+    .fetch_one(executor)
+    .await
+    .unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::blocks::store_block;
+    use super::super::states::store_state;
+    use super::*;
+    use crate::beacon_chain::blocks::GENESIS_PARENT_ROOT;
+    use crate::beacon_chain::node::BeaconHeader;
+    use crate::beacon_chain::node::BeaconHeaderEnvelope;
+    use crate::beacon_chain::node::BeaconHeaderSignedEnvelope;
+    use crate::config;
+    use serial_test::serial;
+    use sqlx::PgConnection;
+
+    async fn clean_tables<'a>(pg_exec: impl PgExecutor<'a>) {
+        sqlx::query("TRUNCATE beacon_states CASCADE")
+            .execute(pg_exec)
+            .await
+            .unwrap();
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn get_deposits_sum_test() {
+        let mut connection: PgConnection = sqlx::Connection::connect(&config::get_db_url())
+            .await
+            .unwrap();
+
+        store_state(&mut connection, "0xstate_root", &0)
+            .await
+            .unwrap();
+
+        store_block(
+            &mut connection,
+            "0xstate_root",
+            &BeaconHeaderSignedEnvelope {
+                root: "0xblock_root".to_string(),
+                header: BeaconHeaderEnvelope {
+                    message: BeaconHeader {
+                        slot: 0,
+                        parent_root: GENESIS_PARENT_ROOT.to_string(),
+                        state_root: "0xstate_root".to_string(),
+                    },
+                },
+            },
+            &GweiAmount(0),
+            &GweiAmount(1),
+        )
+        .await;
+
+        let deposits_sum = get_deposits_sum(&mut connection).await;
+
+        clean_tables(&mut connection).await;
+
+        assert_eq!(
+            deposits_sum,
+            BeaconDepositsSum {
+                deposits_sum: GweiAmount(1),
+                slot: 0
+            }
+        );
+    }
 }
