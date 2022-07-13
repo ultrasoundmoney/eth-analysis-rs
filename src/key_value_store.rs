0b4602b1d03a92b81bc72f07383e7f6bf4ad5655
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-07-13 16:42:45 +0200

    feat: add ability to store json str

--- src/key_value_store.rs
+++ src/key_value_store.rs
@@ -32,7 +32,7 @@ pub struct KeyValue<'a> {
     pub value: Value,
 }
 
-pub async fn set_value<'a>(pg_executor: impl PgExecutor<'a>, key_value: KeyValue<'_>) {
+pub async fn set_value<'a>(executor: impl PgExecutor<'a>, key_value: KeyValue<'_>) {
     tracing::debug!(
         "storing key: {}, value: {:?}",
         &key_value.key,
@@ -48,7 +48,33 @@ pub async fn set_value<'a>(pg_executor: impl PgExecutor<'a>, key_value: KeyValue
         key_value.key,
         key_value.value
     )
-    .execute(pg_executor)
+    .execute(executor)
+    .await
+    .unwrap();
+}
+
+pub struct KeyValueStr<'a> {
+    pub key: &'a str,
+    pub value_str: &'a str,
+}
+
+pub async fn set_value_str<'a>(executor: impl PgExecutor<'a>, key_value: KeyValueStr<'_>) {
+    tracing::debug!(
+        "storing key: {}, value: {:?}",
+        &key_value.key,
+        &key_value.value_str
+    );
+
+    sqlx::query(
+        "
+            INSERT INTO key_value_store (key, value) VALUES ($1, $2::jsonb)
+            ON CONFLICT (key) DO UPDATE SET
+                value = excluded.value
+        ",
+    )
+    .bind(key_value.key)
+    .bind(key_value.value_str)
+    .execute(executor)
     .await
     .unwrap();
 }
@@ -56,6 +82,9 @@ pub async fn set_value<'a>(pg_executor: impl PgExecutor<'a>, key_value: KeyValue
 #[cfg(test)]
 mod tests {
     use serde::Deserialize;
+    use serde_json::json;
+    use serial_test::serial;
+    use sqlx::{Connection, PgConnection};
 
     use crate::config;
 
@@ -68,15 +97,18 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_get_set_value() {
-        let pool = sqlx::PgPool::connect(&config::get_db_url()).await.unwrap();
+    #[serial]
+    async fn get_set_value_test() {
+        let mut connection = PgConnection::connect(&config::get_db_url()).await.unwrap();
+        let mut transaction = connection.begin().await.unwrap();
+
         let test_json = TestJson {
             name: "alex".to_string(),
             age: 29,
         };
 
         set_value(
-            &pool,
+            &mut transaction,
             KeyValue {
                 key: "test-key",
                 value: serde_json::to_value(&test_json).unwrap(),
@@ -84,9 +116,44 @@ mod tests {
         )
         .await;
 
-        let test_json_from_db =
-            serde_json::from_value::<TestJson>(get_value(&pool, "test-key").await.unwrap())
-                .unwrap();
+        let test_json_from_db = serde_json::from_value::<TestJson>(
+            get_value(&mut transaction, "test-key").await.unwrap(),
+        )
+        .unwrap();
+
+        assert_eq!(test_json_from_db, test_json)
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn set_value_str_test() {
+        let mut connection = PgConnection::connect(&config::get_db_url()).await.unwrap();
+        let mut transaction = connection.begin().await.unwrap();
+
+        let test_json = TestJson {
+            name: "alex".to_string(),
+            age: 29,
+        };
+
+        let test_json_str = serde_json::to_string(&json!({
+            "name": "alex",
+            "age": 29
+        }))
+        .unwrap();
+
+        set_value_str(
+            &mut transaction,
+            KeyValueStr {
+                key: "test-key",
+                value_str: &test_json_str,
+            },
+        )
+        .await;
+
+        let test_json_from_db = serde_json::from_value::<TestJson>(
+            get_value(&mut transaction, "test-key").await.unwrap(),
+        )
+        .unwrap();
 
         assert_eq!(test_json_from_db, test_json)
     }
