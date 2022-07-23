8739abdc7779b6b86644f731c96e7e18ee30bf68
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-07-23 15:02:55 +0200

    refactor: cache config key getting

--- src/config.rs
+++ src/config.rs
@@ -1,32 +1,44 @@
 use std::env;
 
-pub fn get_env_key_unsafe(key: &str) -> String {
-    env::var(key).expect(&format!("{key} in env"))
+use cached::proc_macro::cached;
+
+pub fn get_env_var(key: &str) -> Option<String> {
+    let var = match env::var(key) {
+        Err(env::VarError::NotPresent) => None,
+        Err(err) => panic!("{}", err),
+        Ok(var) => Some(var),
+    };
+    tracing::debug!("env var {key}: {var:?}");
+    var
 }
 
-#[allow(dead_code)]
-pub fn get_env_key(key: &str) -> Result<String, env::VarError> {
-    env::var(key)
+pub fn get_env_var_unsafe(key: &str) -> String {
+    get_env_var(key).expect(&format!("{key} in env"))
 }
 
+#[cached]
 pub fn get_beacon_url() -> String {
-    get_env_key_unsafe("BEACON_URL")
+    get_env_var_unsafe("BEACON_URL")
 }
 
+#[cached]
 pub fn get_db_url() -> String {
-    get_env_key_unsafe("DATABASE_URL")
+    get_env_var_unsafe("DATABASE_URL")
 }
 
+#[cached]
 pub fn get_glassnode_api_key() -> String {
-    get_env_key_unsafe("GLASSNODE_API_KEY")
+    get_env_var_unsafe("GLASSNODE_API_KEY")
 }
 
+#[cached]
 pub fn get_etherscan_api_key() -> String {
-    get_env_key_unsafe("ETHERSCAN_API_KEY")
+    get_env_var_unsafe("ETHERSCAN_API_KEY")
 }
 
+#[cached]
 pub fn get_execution_url() -> String {
-    get_env_key_unsafe("GETH_URL")
+    get_env_var_unsafe("GETH_URL")
 }
 
 #[cfg(test)]
@@ -35,29 +47,30 @@ mod tests {
 
     #[test]
     #[should_panic]
-    fn test_get_key_unsafe_panics() {
-        get_env_key_unsafe("DOESNT_EXIST");
+    fn get_env_var_unsafe_panics_test() {
+        get_env_var_unsafe("DOESNT_EXIST");
     }
 
     #[test]
-    fn test_get_key_unsafe_returns() {
-        let test_key = "TEST_KEY";
+    fn get_env_var_unsafe_test() {
+        let test_key = "TEST_KEY_UNSAFE";
         let test_value = "my-env-value";
         std::env::set_var(test_key, test_value);
-        assert_eq!(get_env_key_unsafe(test_key), test_value);
+        assert_eq!(get_env_var_unsafe(test_key), test_value);
     }
 
     #[test]
-    fn test_get_key_safe_some() {
-        let test_key = "TEST_KEY";
+    fn get_env_var_safe_some_test() {
+        let test_key = "TEST_KEY_SAFE_SOME";
         let test_value = "my-env-value";
         std::env::set_var(test_key, test_value);
-        assert_eq!(get_env_key(test_key), Ok(test_value.to_string()));
+        assert_eq!(get_env_var(test_key), Some(test_value.to_string()));
     }
 
     #[test]
-    fn test_get_key_safe_none() {
-        let key = get_env_key("DOESNT_EXIST");
-        assert!(key.is_err());
+    fn get_env_var_safe_none_test() {
+        let key = get_env_var("DOESNT_EXIST");
+        assert!(key.is_none());
+    }
     }
 }
