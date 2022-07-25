796c199d6d4b16171c683242f80124cfb4513fee
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-07-26 00:06:39 +0200

    fix: add missing snapshot module

---
+++ src/execution_chain/supply_deltas/snapshot.rs
@@ -0,0 +1,18 @@
+use super::Wei;
+
+pub struct SupplySnapshot {
+    #[allow(dead_code)]
+    pub accounts_count: u64,
+    pub block_hash: &'static str,
+    pub block_number: u32,
+    pub root: &'static str,
+    pub balances_sum: Wei,
+}
+
+pub const SUPPLY_SNAPSHOT_15082718: SupplySnapshot = SupplySnapshot {
+    accounts_count: 176_496_428,
+    block_hash: "0xba7baa960085d0997884135a9c0f04f6b6de53164604084be701f98a31c4124d",
+    block_number: 15_082_718,
+    root: "655618..cfe0e6",
+    balances_sum: 118908973575220938641041929,
+};
