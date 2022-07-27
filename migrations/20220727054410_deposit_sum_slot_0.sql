17898d7e44fd2d7e08cbd67824e7119dbd0b214e
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-07-27 11:57:23 +0200

    refactor: make deposit migration a lot faster

--- migrations/20220727054410_deposit_sum_slot_0.sql
+++ migrations/20220727054410_deposit_sum_slot_0.sql
@@ -1,6 +1,29 @@
+-- For performance reasons, rather than updating 4M rows, we create a tmp table and recreate indexes.
+-- Make sure no writes happen to beacon_blocks and not to beacon_blocks_tmp while we update.
+LOCK TABLE beacon_blocks IN EXCLUSIVE MODE;
+
+-- Set deposit sum for slot 0.
 UPDATE beacon_blocks
 SET deposit_sum = 674144000000000
 WHERE state_root = '0x7e76880eb67bbdc86250aa578958e9d0675e64e714337855204fb5abaaf82c2b';
 
-UPDATE beacon_blocks
-SET deposit_sum_aggregated = deposit_sum_aggregated + 674144000000000;
+CREATE TABLE beacon_blocks_tmp AS (
+  SELECT
+    block_root,
+    state_root,
+    parent_root,
+    deposit_sum,
+    (deposit_sum_aggregated + 674144000000000) AS deposit_sum_aggregated
+  FROM
+    beacon_blocks
+);
+
+-- Swap the tables around.
+DROP TABLE beacon_blocks;
+ALTER TABLE beacon_blocks_tmp RENAME TO beacon_blocks;
+
+-- Recreate the indices.
+ALTER TABLE public.beacon_blocks ADD CONSTRAINT beacon_blocks_pk PRIMARY KEY (block_root);
+ALTER TABLE public.beacon_blocks ADD CONSTRAINT beacon_blocks_state_root_fk FOREIGN KEY (state_root) REFERENCES beacon_states(state_root);
+CREATE UNIQUE INDEX beacon_blocks_state_root_idx ON public.beacon_blocks USING btree (state_root);
+
