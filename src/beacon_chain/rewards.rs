018acaf82bb76bafd940801e75e5115bf86887c5
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-08 19:43:11 +0200

    refactor: remove unneeded var

--- src/beacon_chain/rewards.rs
+++ src/beacon_chain/rewards.rs
@@ -77,9 +77,7 @@ const BASE_REWARD_FACTOR: u8 = 64;
 
 // Consider staying in Gwei until the last moment instead of converting early.
 pub fn get_issuance_reward(GweiAmount(effective_balance_sum): GweiAmount) -> ValidatorReward {
-    let total_effective_balance = effective_balance_sum as f64;
-
-    let active_validators = total_effective_balance / GWEI_PER_ETH_F64 / 32f64;
+    let active_validators = effective_balance_sum as f64 / GWEI_PER_ETH_F64 / 32f64;
 
     // Balance at stake (Gwei)
     let max_balance_at_stake = active_validators * MAX_EFFECTIVE_BALANCE;
@@ -90,11 +88,11 @@ pub fn get_issuance_reward(GweiAmount(effective_balance_sum): GweiAmount) -> Val
     let max_issuance_per_year = max_issuance_per_epoch * EPOCHS_PER_YEAR;
 
     let annual_reward = max_issuance_per_year / active_validators;
-    let apr = max_issuance_per_year / total_effective_balance;
+    let apr = max_issuance_per_year / effective_balance_sum as f64;
 
     tracing::debug!(
         "total effective balance: {} ETH",
-        total_effective_balance / GWEI_PER_ETH_F64
+        effective_balance_sum as f64 / GWEI_PER_ETH_F64
     );
     tracing::debug!("nr of active validators: {}", active_validators);
     tracing::debug!(
@@ -105,7 +103,7 @@ pub fn get_issuance_reward(GweiAmount(effective_balance_sum): GweiAmount) -> Val
         "max issuance per year: {} ETH",
         max_issuance_per_year / GWEI_PER_ETH_F64
     );
-    tracing::debug!("APR: {:.4}%", apr);
+    tracing::debug!("APR: {:.2}%", apr * 100f64);
 
     ValidatorReward {
         annual_reward: GweiAmount(annual_reward as u64),
