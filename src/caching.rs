931df3acbacf6cd8732b15852aeb9ac43b3dbae2
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-08 19:41:19 +0200

    refactor: simplify quotes

--- src/caching.rs
+++ src/caching.rs
@@ -4,9 +4,9 @@ pub async fn publish_cache_update<'a>(pool: impl PgExecutor<'a>, key: &str) {
     tracing::debug!("publishing cache update: {}", key);
 
     sqlx::query!(
-        r#"
+        "
             SELECT pg_notify('cache-update', $1)
-        "#,
+        ",
         key
     )
     .execute(pool)
