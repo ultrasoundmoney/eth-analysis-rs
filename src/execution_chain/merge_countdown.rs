6d09a0f9287e3b1d76e7e1b5d7f5f42b00f944b8
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-12 21:44:07 +0200

    feat: add blocks left, rename key

--- src/execution_chain/merge_countdown.rs
+++ src/execution_chain/merge_countdown.rs
@@ -12,41 +12,43 @@ use super::node::{BlockNumber, Difficulty, ExecutionNodeBlock, TotalDifficulty};
 
 const TOTAL_TERMINAL_DIFFICULTY: u128 = 58750000000000000000000;
 
-const MERGE_TTD_COUNTDOWN_CACHE_KEY: &str = "merge-ttd-countdown";
+const MERGE_ESTIMATE_CACHE_KEY: &str = "merge-estimate";
 
 #[derive(Serialize)]
 struct MergeTTDCountdown {
     block_number: BlockNumber,
+    blocks_left: u32,
+    #[serde(serialize_with = "to_u64_string")]
     difficulty: Difficulty,
-    total_difficulty: TotalDifficulty,
     estimated_date_time: DateTime<Utc>,
-}
-
-fn estimate_merge(block: &ExecutionNodeBlock) -> DateTime<Utc> {
-    let blocks_left =
-        (TOTAL_TERMINAL_DIFFICULTY - block.total_difficulty) / block.difficulty as u128;
-    let time_left = Duration::seconds(13).mul(blocks_left as i32);
-    Utc::now() + time_left
+    #[serde(serialize_with = "to_u128_string")]
+    total_difficulty: TotalDifficulty,
 }
 
 pub async fn on_new_head(executor: &PgPool, block: &ExecutionNodeBlock) {
     tracing::debug!("updating merge TTD countdown");
 
+    let blocks_left =
+        ((TOTAL_TERMINAL_DIFFICULTY - block.total_difficulty) / block.difficulty as u128) as u32;
+    let time_left = Duration::seconds(13).mul(blocks_left as i32);
+    let estimated_date_time = Utc::now() + time_left;
+
     let merge_ttd_countdown = MergeTTDCountdown {
         block_number: block.number,
+        blocks_left,
         difficulty: block.difficulty,
+        estimated_date_time,
         total_difficulty: block.total_difficulty,
-        estimated_date_time: estimate_merge(block),
     };
 
     key_value_store::set_value(
         executor,
         KeyValue {
-            key: MERGE_TTD_COUNTDOWN_CACHE_KEY,
+            key: MERGE_ESTIMATE_CACHE_KEY,
             value: serde_json::to_value(merge_ttd_countdown).unwrap(),
         },
     )
     .await;
 
-    caching::publish_cache_update(executor, MERGE_TTD_COUNTDOWN_CACHE_KEY).await;
+    caching::publish_cache_update(executor, MERGE_ESTIMATE_CACHE_KEY).await;
 }
