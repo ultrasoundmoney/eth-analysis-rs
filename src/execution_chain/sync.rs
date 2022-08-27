c24b8b1fbd4ea968def97fce1b5d6482b4cc05bb
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-28 01:19:34 +0200

    feat: sync from last on start

--- src/execution_chain/sync.rs
+++ src/execution_chain/sync.rs
@@ -6,8 +6,11 @@
 //! code, adding more tests, and improving designs. This side should slowly take over more
 //! responsibilities.
 
+use futures::SinkExt;
+use futures::Stream;
 use futures::StreamExt;
 use sqlx::PgPool;
+use std::cmp::Ordering;
 use std::collections::VecDeque;
 use std::iter::Iterator;
 use std::sync::Arc;
@@ -146,6 +149,112 @@ async fn sync_head(
     }
 }
 
+#[derive(Clone)]
+pub struct BlockRange {
+    greater_than_or_equal: BlockNumber,
+    less_than_or_equal: BlockNumber,
+}
+
+impl BlockRange {
+    pub fn new(greater_than_or_equal: u32, less_than_or_equal: u32) -> Self {
+        if greater_than_or_equal > less_than_or_equal {
+            panic!("tried to create slot range with negative range")
+        }
+
+        Self {
+            greater_than_or_equal,
+            less_than_or_equal,
+        }
+    }
+}
+
+pub struct BlockRangeIntoIterator {
+    block_range: BlockRange,
+    index: usize,
+}
+
+impl IntoIterator for BlockRange {
+    type Item = u32;
+    type IntoIter = BlockRangeIntoIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Self::IntoIter {
+            block_range: self,
+            index: 0,
+        }
+    }
+}
+
+impl Iterator for BlockRangeIntoIterator {
+    type Item = BlockNumber;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match (self.block_range.greater_than_or_equal as usize + self.index)
+            .cmp(&(self.block_range.less_than_or_equal as usize))
+        {
+            Ordering::Less => {
+                let current = self.block_range.greater_than_or_equal + self.index as u32;
+                self.index = self.index + 1;
+                Some(current)
+            }
+            Ordering::Equal => {
+                let current = self.block_range.greater_than_or_equal + self.index as u32;
+                self.index = self.index + 1;
+                Some(current)
+            }
+            Ordering::Greater => None,
+        }
+    }
+}
+
+fn get_historic_stream(block_range: BlockRange) -> impl Stream<Item = Head> {
+    let (mut tx, rx) = futures::channel::mpsc::channel(10);
+
+    tokio::spawn(async move {
+        let mut execution_node = ExecutionNode::connect().await;
+        for block_number in block_range.into_iter() {
+            let block = execution_node
+                .get_block_by_number(&block_number)
+                .await
+                .unwrap();
+            tx.send(block.into()).await.unwrap();
+        }
+    });
+
+    rx
+}
+
+async fn stream_heads_from(gte_slot: BlockNumber) -> impl Stream<Item = Head> {
+    tracing::debug!("streaming heads from {gte_slot}");
+
+    let mut execution_node = ExecutionNode::connect().await;
+    let last_block_on_start = execution_node.get_latest_block().await;
+    tracing::debug!("last block on chain: {}", &last_block_on_start.number);
+
+    // We stream heads as requested until caught up with the chain and then pass heads as they come
+    // in from our node. The only way to be sure how high we should stream, is to wait for the
+    // first head from the node to come in. We don't want to wait. So ask for the latest head, take
+    // this as the max, and immediately start listening for new heads. Running the small risk the
+    // chain has advanced between these two calls.
+    let heads_stream = execution_chain::stream_new_heads();
+
+    let block_range = BlockRange::new(gte_slot, last_block_on_start.number - 1);
+
+    let historic_heads_stream = get_historic_stream(block_range);
+
+    historic_heads_stream.chain(heads_stream)
+}
+
+async fn stream_heads_from_last(db: &PgPool) -> impl Stream<Item = Head> {
+    let mut connection = db.acquire().await.unwrap();
+    let mut block_store = BlockStore::new(&mut *connection);
+    let next_block_to_sync = block_store
+        .get_last_block_number()
+        .await
+        .map_or(15253306, |number| number + 1);
+    stream_heads_from(next_block_to_sync).await
+}
+
 #[derive(Clone, Debug)]
 enum HeadToSync {
     Fetched(Head),
@@ -164,12 +273,12 @@ pub async fn sync_blocks() {
 
     let mut execution_node = ExecutionNode::connect().await;
 
-    let mut heads_stream = execution_chain::stream_new_heads();
-
-    let heads_queue: HeadsQueue = Arc::new(Mutex::new(VecDeque::new()));
-
     let mut connection = db_pool.acquire().await.unwrap();
     let mut block_store = BlockStore::new(&mut connection);
+
+    let mut heads_stream = stream_heads_from_last(&db_pool).await;
+
+    let heads_queue: HeadsQueue = Arc::new(Mutex::new(VecDeque::new()));
 
     while let Some(head_event) = heads_stream.next().await {
         heads_queue
@@ -256,5 +365,11 @@ mod tests {
         // This should blow up if the order is backwards but its not obvious how. Consider using
         // mockall to create a mock instance of block_store so we can observe whether
         // rollback_numbers is calling it correctly.
+    }
+
+    #[test]
+    fn block_range_iterable_test() {
+        let range = (BlockRange::new(1, 4)).into_iter().collect::<Vec<u32>>();
+        assert_eq!(range, vec![1, 2, 3, 4]);
     }
 }
