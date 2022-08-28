422582ce711a6e75d152b5723f345cc55ac96f9d
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-28 04:39:42 +0200

    fix: test db connection

--- src/db_testing.rs
+++ src/db_testing.rs
@@ -1,11 +1,12 @@
-use sqlx::postgres::PgConnectOptions;
-use sqlx::ConnectOptions;
+use sqlx::Connection;
 use sqlx::PgConnection;
 
+use crate::config;
+
 pub async fn get_test_db() -> PgConnection {
-    PgConnectOptions::new()
-        .database("testdb")
-        .connect()
-        .await
-        .unwrap()
+    if !config::get_db_url().contains("testdb") {
+        panic!("tried to run tests against db that is not 'testdb'");
+    }
+
+    Connection::connect(&config::get_db_url()).await.unwrap()
 }
