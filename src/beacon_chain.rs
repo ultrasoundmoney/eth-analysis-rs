5011e85bf256aa0100554e3a9e65e45759075039
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-09-14 20:17:46 +0100

    feat: more pub beacon structs

--- src/beacon_chain.rs
+++ src/beacon_chain.rs
@@ -11,16 +11,20 @@ mod states;
 mod sync;
 
 pub use balances::{get_validator_balances_by_start_of_day, BeaconBalancesSum};
+pub use blocks::store_block;
+pub use blocks::GENESIS_PARENT_ROOT;
 pub use deposits::{get_deposits_sum, BeaconDepositsSum};
-pub use effective_balance_sum::{
-    get_last_stored_effective_balance_sum, update_effective_balance_sum,
-};
+pub use effective_balance_sum::get_last_stored_effective_balance_sum;
+pub use effective_balance_sum::update_effective_balance_sum;
 pub use issuance::get_current_issuance;
 pub use issuance::get_issuance_by_start_of_day;
 pub use issuance::get_last_week_issuance;
 pub use merge_stats::update_merge_stats_by_hand;
+pub use node::BeaconHeader;
+pub use node::BeaconHeaderEnvelope;
+pub use node::BeaconHeaderSignedEnvelope;
 pub use node::BeaconNode;
 pub use rewards::update_validator_rewards;
-pub use states::Slot;
 pub use states::store_state;
+pub use states::Slot;
 pub use sync::sync_beacon_states;
