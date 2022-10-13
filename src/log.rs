8573948737d3cdadcbb2e3e55b715f9fd5503c15
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-10-13 09:42:33 -0500

    feat: use env filter with json

--- src/log.rs
+++ src/log.rs
@@ -2,8 +2,14 @@ use crate::env::{self, Env};
 
 pub fn init_with_env() {
     match *env::ENV {
-        Env::Dev => tracing_subscriber::fmt::init(),
-        Env::Prod => tracing_subscriber::fmt::fmt().json().init(),
-        Env::Stag => tracing_subscriber::fmt::fmt().json().init(),
+        Env::Dev => fmt::init(),
+        Env::Prod => tracing_subscriber::registry()
+            .with(Layer::default().json())
+            .with(EnvFilter::from_default_env())
+            .init(),
+        Env::Stag => tracing_subscriber::registry()
+            .with(Layer::default().json())
+            .with(EnvFilter::from_default_env())
+            .init(),
     }
 }
