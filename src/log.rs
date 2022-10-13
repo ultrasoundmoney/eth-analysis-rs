d5743ce89c23774f563fa6a16bc8da56b852f698
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-10-13 11:11:59 -0500

    feat: pretty print

--- src/log.rs
+++ src/log.rs
@@ -1,3 +1,4 @@
+use lazy_static::lazy_static;
 use tracing_subscriber::{
     fmt::{self, Layer},
     prelude::__tracing_subscriber_SubscriberExt,
@@ -7,16 +8,24 @@ use tracing_subscriber::{
 
 use crate::env::{self, Env};
 
+lazy_static! {
+    static ref PRETTY_PRINT: bool = env::get_env_bool("PRETTY_PRINT");
+}
+
 pub fn init_with_env() {
-    match *env::ENV {
-        Env::Dev => fmt::init(),
-        Env::Prod => tracing_subscriber::registry()
-            .with(Layer::default().json())
-            .with(EnvFilter::from_default_env())
-            .init(),
-        Env::Stag => tracing_subscriber::registry()
-            .with(Layer::default().json())
-            .with(EnvFilter::from_default_env())
-            .init(),
+    if *PRETTY_PRINT {
+        fmt::init();
+    } else {
+        match *env::ENV {
+            Env::Dev => fmt::init(),
+            Env::Prod => tracing_subscriber::registry()
+                .with(Layer::default().json())
+                .with(EnvFilter::from_default_env())
+                .init(),
+            Env::Stag => tracing_subscriber::registry()
+                .with(Layer::default().json())
+                .with(EnvFilter::from_default_env())
+                .init(),
+        }
     }
 }
