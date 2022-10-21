019da6113135ef902d8e3a538bfd29639d094346
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-10-20 21:20:54 -0500

    feat: parse and write timeframes

--- src/time_frames.rs
+++ src/time_frames.rs
@@ -1,6 +1,15 @@
-use chrono::Duration;
-use sqlx::postgres::types::PgInterval;
+use std::{fmt::Display, slice::Iter, str::FromStr};
 
+use chrono::Duration;
+use sqlx::{
+    postgres::{types::PgInterval, PgRow},
+    PgExecutor, Row,
+};
+use thiserror::Error;
+
+use crate::execution_chain::BlockNumber;
+
+#[derive(Debug, PartialEq)]
 pub enum LimitedTimeFrame {
     Day1,
     Day30,
@@ -24,6 +33,41 @@ impl From<LimitedTimeFrame> for Duration {
 impl From<LimitedTimeFrame> for PgInterval {
     fn from(limited_time_frame: LimitedTimeFrame) -> Self {
         PgInterval::try_from(Into::<Duration>::into(limited_time_frame)).unwrap()
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ParseTimeFrameError {
+    #[error("failed to parse time frame {0}")]
+    UnknownTimeFrame(String),
+}
+
+impl FromStr for LimitedTimeFrame {
+    type Err = ParseTimeFrameError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "m5" => Ok(LimitedTimeFrame::Minute5),
+            "h1" => Ok(LimitedTimeFrame::Hour1),
+            "d1" => Ok(LimitedTimeFrame::Day1),
+            "d7" => Ok(LimitedTimeFrame::Day7),
+            "d30" => Ok(LimitedTimeFrame::Day30),
+            unknown_time_frame => Err(ParseTimeFrameError::UnknownTimeFrame(
+                unknown_time_frame.to_string(),
+            )),
+        }
+    }
+}
+
+impl Display for LimitedTimeFrame {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use LimitedTimeFrame::*;
+        match self {
+            Day1 => write!(f, "d1"),
+            Day30 => write!(f, "d30"),
+            Day7 => write!(f, "d7"),
+            Hour1 => write!(f, "h1"),
+            Minute5 => write!(f, "m5"),
+        }
     }
 }
 
@@ -57,8 +101,21 @@ impl LimitedTimeFrame {
             },
         }
     }
+
+    pub fn to_db_key(&self) -> &'_ str {
+        use LimitedTimeFrame::*;
+
+        match self {
+            Day1 => "d1",
+            Day30 => "d1",
+            Day7 => "d7",
+            Hour1 => "h1",
+            Minute5 => "m5",
+        }
+    }
 }
 
+#[derive(Debug, PartialEq)]
 pub enum TimeFrame {
     #[allow(dead_code)]
     All,
@@ -68,6 +125,20 @@ pub enum TimeFrame {
 impl From<LimitedTimeFrame> for TimeFrame {
     fn from(limited_time_frame: LimitedTimeFrame) -> Self {
         TimeFrame::LimitedTimeFrame(limited_time_frame)
+    }
+}
+
+impl FromStr for TimeFrame {
+    type Err = ParseTimeFrameError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "all" => Ok(TimeFrame::All),
+            unknown_time_frame => match unknown_time_frame.parse::<LimitedTimeFrame>() {
+                Ok(limited_time_frame) => Ok(TimeFrame::LimitedTimeFrame(limited_time_frame)),
+                Err(err) => Err(err),
+            },
+        }
     }
 }
 
@@ -83,5 +154,90 @@ impl TimeFrame {
                 LimitedTimeFrame::Minute5 => 0.78125,
             },
         }
+    }
+
+    pub fn to_db_key(&self) -> &'_ str {
+        use TimeFrame::*;
+        match self {
+            All => "all",
+            LimitedTimeFrame(limited_time_frame) => limited_time_frame.to_db_key(),
+        }
+    }
+}
+
+static TIME_FRAMES: [TimeFrame; 6] = [
+    TimeFrame::LimitedTimeFrame(LimitedTimeFrame::Minute5),
+    TimeFrame::LimitedTimeFrame(LimitedTimeFrame::Hour1),
+    TimeFrame::LimitedTimeFrame(LimitedTimeFrame::Day1),
+    TimeFrame::LimitedTimeFrame(LimitedTimeFrame::Day7),
+    TimeFrame::LimitedTimeFrame(LimitedTimeFrame::Day30),
+    TimeFrame::All,
+];
+
+impl TimeFrame {
+    pub fn iterator() -> Iter<'static, TimeFrame> {
+        TIME_FRAMES.iter()
+    }
+}
+
+pub async fn get_earliest_block_number(
+    executor: impl PgExecutor<'_>,
+    limited_time_frame: &LimitedTimeFrame,
+) -> sqlx::Result<Option<BlockNumber>> {
+    sqlx::query(
+        "
+            SELECT
+                block_number
+            FROM
+                blocks_next
+            AND
+                timestamp >= NOW() - $1
+        ",
+    )
+    .bind(limited_time_frame.get_postgres_interval())
+    .map(|row: PgRow| row.get::<i32, _>("block_number").try_into().unwrap())
+    .fetch_optional(executor)
+    .await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn time_frame_iter_test() {
+        let time_frames = TimeFrame::iterator().collect::<Vec<&TimeFrame>>();
+        let expected = vec![
+            &TimeFrame::LimitedTimeFrame(LimitedTimeFrame::Minute5),
+            &TimeFrame::LimitedTimeFrame(LimitedTimeFrame::Hour1),
+            &TimeFrame::LimitedTimeFrame(LimitedTimeFrame::Day1),
+            &TimeFrame::LimitedTimeFrame(LimitedTimeFrame::Day7),
+            &TimeFrame::LimitedTimeFrame(LimitedTimeFrame::Day30),
+            &TimeFrame::All,
+        ];
+
+        assert_eq!(expected, time_frames);
+    }
+
+    #[test]
+    fn parse_test() {
+        let time_frame = "all".parse::<TimeFrame>().unwrap();
+        assert_eq!(time_frame, TimeFrame::All);
+
+        let limited_time_frame = "d30".parse::<TimeFrame>().unwrap();
+        assert_eq!(
+            limited_time_frame,
+            TimeFrame::LimitedTimeFrame(LimitedTimeFrame::Day30)
+        )
+    }
+
+    #[test]
+    fn to_db_key_test() {
+        let time_frame_key = TimeFrame::All.to_db_key();
+        assert_eq!(time_frame_key, "all");
+
+        let limited_time_frame_key =
+            TimeFrame::LimitedTimeFrame(LimitedTimeFrame::Day1).to_db_key();
+        assert_eq!(limited_time_frame_key, "d1");
     }
 }
