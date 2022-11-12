34746bc3a2f71c890d38ffd5b8b79b0bca87e773
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-11-12 12:27:20 -0600

    feat(basefeestats): add since_merge
    Adds the average base fees per gas for the since_merge time frame.
    Adds the min, max base fees per gas for the since_merge time frame.

--- src/execution_chain/base_fees/stats.rs
+++ src/execution_chain/base_fees/stats.rs
@@ -17,9 +17,23 @@ async fn get_base_fee_per_gas_average(
     time_frame: &TimeFrame,
 ) -> sqlx::Result<WeiF64> {
     match time_frame {
-        TimeFrame::SinceMerge => unimplemented!(),
+        TimeFrame::SinceMerge => {
+            sqlx::query(
+                "
+                    SELECT
+                        SUM(base_fee_per_gas::FLOAT8 * gas_used::FLOAT8) / SUM(gas_used::FLOAT8) AS average
+                    FROM
+                        blocks
+                    WHERE
+                        timestamp >= '2022-09-15T06:42:42Z'::TIMESTAMPTZ
+                ",
+            )
+            .map(|row: PgRow| row.get::<f64, _>("average"))
+            .fetch_one(executor)
+            .await
+        },
         TimeFrame::SinceBurn => {
-            warn!("getting average fee for time frame 'all' is slow, and may be incorrect depending on blocks_next backfill status");
+            warn!("getting average fee for time frame 'since_burn' is slow, and may be incorrect depending on blocks_next backfill status");
             sqlx::query(
                 "
                     SELECT
@@ -62,7 +76,26 @@ async fn get_base_fee_per_gas_min_max(
     time_frame: &TimeFrame,
 ) -> sqlx::Result<BaseFeePerGasMinMax> {
     match time_frame {
-        TimeFrame::SinceMerge => unimplemented!(),
+        TimeFrame::SinceMerge => {
+            sqlx::query(
+                "
+                    SELECT
+                        MIN(base_fee_per_gas),
+                        MAX(base_fee_per_gas)
+                    FROM
+                        blocks
+                    WHERE
+                        timestamp >= '2022-09-15T06:42:42Z'::TIMESTAMPTZ
+                ",
+            )
+            .map(|row: PgRow| {
+                let min = row.get::<i64, _>("min") as f64;
+                let max = row.get::<i64, _>("max") as f64;
+                BaseFeePerGasMinMax { min, max }
+            })
+            .fetch_one(executor)
+            .await
+        }
         TimeFrame::SinceBurn => {
             sqlx::query(
                 "
@@ -147,7 +180,7 @@ pub async fn update_base_fee_stats(
 
     debug!("barrier: {barrier}");
 
-    let (m5, h1, d1, d7, d30, all) = try_join!(
+    let (m5, h1, d1, d7, d30, since_burn, since_merge) = try_join!(
         get_base_fee_per_gas_stats_time_frame(
             executor,
             &TimeFrame::Limited(LimitedTimeFrame::Minute5),
@@ -168,11 +201,12 @@ pub async fn update_base_fee_stats(
             executor,
             &TimeFrame::Limited(LimitedTimeFrame::Day30),
         ),
-        get_base_fee_per_gas_stats_time_frame(executor, &TimeFrame::SinceBurn)
+        get_base_fee_per_gas_stats_time_frame(executor, &TimeFrame::SinceBurn),
+        get_base_fee_per_gas_stats_time_frame(executor, &TimeFrame::SinceMerge)
     )?;
 
     let base_fee_per_gas_stats = BaseFeePerGasStats {
-        all: Some(all.clone()),
+        all: Some(since_burn.clone()),
         barrier,
         block_number: block.number,
         d1,
@@ -181,8 +215,8 @@ pub async fn update_base_fee_stats(
         h1,
         m5,
         timestamp: block.timestamp,
-        since_burn: all,
-        since_merge: None,
+        since_burn,
+        since_merge: Some(since_merge),
     };
 
     key_value_store::set_value(
