54e0bbdc02bd81f2450648e236ef6af4ab48b0c0
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-11-18 15:22:45 -0600

    feat(beaconstate): speed up state_root check
    Fetches stored state_roots all at once to eliminate roundtrip latency
    for ~5M rows.

--- src/beacon_chain/states/heal.rs
+++ src/beacon_chain/states/heal.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{
     beacon_chain::{sync, Slot},
     key_value_store,
@@ -44,30 +46,48 @@ pub async fn heal_beacon_states() -> Result<()> {
         .expect("a beacon state should be stored before trying to heal any")
         .slot;
     let last_checked = get_last_checked(&db_pool).await?;
-    let earliest = last_checked.unwrap_or(FIRST_STORED_ETH_SUPPLY_SLOT);
+    let starting_slot = last_checked.unwrap_or(FIRST_STORED_ETH_SUPPLY_SLOT);
 
     debug!(
-        earliest,
+        starting_slot,
         last_slot, "checking first stored slot to last slot for gaps"
     );
 
-    let mut progress = Progress::new("heal-beacon-states", (last_slot - earliest).into());
+    let mut progress = Progress::new("heal-beacon-states", (last_slot - starting_slot).into());
 
-    for slot in earliest..=last_slot {
+    let stored_state_roots = sqlx::query!(
+        "
+            SELECT
+                slot,
+                state_root
+            FROM
+                beacon_states
+            WHERE
+                slot >= $1
+        ",
+        starting_slot as i32
+    )
+    .fetch_all(&db_pool)
+    .await?
+    .iter()
+    .map(|row| (row.slot as u32, row.state_root.clone()))
+    .collect::<HashMap<u32, String>>();
+
+    for slot in starting_slot..=last_slot {
         if slot % 100 == 0 {
             store_last_checked(&db_pool, &slot).await?;
             info!("{}", progress.get_progress_string());
         }
 
-        let stored_state_root = beacon_chain::get_state_root_by_slot(&db_pool, &slot)
-            .await?
+        let stored_state_root = stored_state_roots
+            .get(&slot)
             .expect("expect some historic state_root to have been stored");
         let state_root = beacon_node
             .get_state_root_by_slot(&slot)
             .await?
             .expect("expect state_root to exist for historic slots");
 
-        if stored_state_root != state_root {
+        if *stored_state_root != state_root {
             warn!("state root mismatch, rolling back stored and resyncing");
             sync::rollback_slot(&mut *db_pool.acquire().await?, &slot).await?;
             sync::sync_state_root(&db_pool, &beacon_node, &state_root, &slot).await?;
