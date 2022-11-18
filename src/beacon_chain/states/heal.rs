860109cc26ee844597e2aee6650249ddafa89af1
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-11-18 09:27:43 -0600

    fix(beaconstate): log typo

--- src/beacon_chain/states/heal.rs
+++ src/beacon_chain/states/heal.rs
@@ -68,7 +68,7 @@ pub async fn heal_beacon_states() -> Result<()> {
             .expect("expect state_root to exist for historic slots");
 
         if stored_state_root != state_root {
-            warn!("state roon mismatch, rolling back stored and resyncing");
+            warn!("state root mismatch, rolling back stored and resyncing");
             sync::rollback_slot(&mut *db_pool.acquire().await?, &slot).await?;
             sync::sync_state_root(&db_pool, &beacon_node, &state_root, &slot).await?;
             info!(slot, "healed state at slot");
