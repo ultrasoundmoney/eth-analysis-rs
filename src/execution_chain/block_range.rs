7651e41e72aee3be8a37c6ed47d4018865ef3750
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-01-30 17:34:49 +0700

    refactor(block_range): remove iterator comparison

--- src/execution_chain/block_range.rs
+++ src/execution_chain/block_range.rs
@@ -1,21 +1,19 @@
 use super::BlockNumber;
 
+/// A range of blocks. The range is inclusive of both the lowest and highest.
 #[derive(Clone)]
 pub struct BlockRange {
-    pub greater_than_or_equal: BlockNumber,
-    pub less_than_or_equal: BlockNumber,
+    pub lowest: BlockNumber,
+    pub highest: BlockNumber,
 }
 
 impl BlockRange {
-    pub fn new(greater_than_or_equal: BlockNumber, less_than_or_equal: BlockNumber) -> Self {
-        if greater_than_or_equal > less_than_or_equal {
-            panic!("tried to create slot range with negative range")
+    pub fn new(lowest: BlockNumber, highest: BlockNumber) -> Self {
+        if lowest > highest {
+            panic!("tried to create negative block range")
         }
 
-        Self {
-            greater_than_or_equal,
-            less_than_or_equal,
-        }
+        Self { lowest, highest }
     }
 }
 
@@ -40,20 +38,12 @@ impl Iterator for BlockRangeIntoIterator {
     type Item = BlockNumber;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match (self.block_range.greater_than_or_equal + self.index as BlockNumber)
-            .cmp(&(self.block_range.less_than_or_equal))
-        {
-            Ordering::Less => {
-                let current = self.block_range.greater_than_or_equal + self.index as BlockNumber;
-                self.index += 1;
-                Some(current)
-            }
-            Ordering::Equal => {
-                let current = self.block_range.greater_than_or_equal + self.index as BlockNumber;
-                self.index += 1;
-                Some(current)
-            }
-            Ordering::Greater => None,
+        if self.block_range.lowest + self.index as BlockNumber <= self.block_range.highest {
+            let block_number: BlockNumber = self.block_range.lowest + self.index as BlockNumber;
+            self.index += 1;
+            Some(block_number)
+        } else {
+            None
         }
     }
 }
