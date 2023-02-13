9c3ff67666513c1c91a94031ecc49842d3538099
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-02-13 14:58:15 +0700

    feat(gauges): add proof of work simulation

--- src/gauges.rs
+++ src/gauges.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use enum_iterator::all;
+use futures::join;
 use serde::Serialize;
 use sqlx::PgPool;
 
@@ -10,8 +11,14 @@ use crate::{
     caching::{self, CacheKey},
     execution_chain::ExecutionNodeBlock,
     time_frames::TimeFrame,
-    units::EthNewtype,
+    units::{EthNewtype, UsdNewtype},
+    usd_price,
 };
+
+const PROOF_OF_WORK_DAILY_ISSUANCE_ESTIMATE: f64 = 13500.0;
+const DAYS_PER_YEAR: f64 = 365.25;
+const PROOF_OF_WORK_YEARLY_ISSUANCE_ESTIMATE: f64 =
+    PROOF_OF_WORK_DAILY_ISSUANCE_ESTIMATE * DAYS_PER_YEAR;
 
 #[derive(Debug, Serialize)]
 pub struct GaugeRatesTimeFrame {
@@ -24,11 +31,6 @@ pub struct GaugeRatesTimeFrame {
 
 pub type GaugeRates = HashMap<TimeFrame, GaugeRatesTimeFrame>;
 
-// Collect the burn rates.
-// Collect the issuance rates.
-// Collect the supply growth rates.
-// Build the proof of work simulations based on the issuance rate.
-// Build the proof of work simulations based on the supply growth rate.
 pub async fn on_new_block(
     db_pool: &PgPool,
     block: &ExecutionNodeBlock,
@@ -43,11 +45,27 @@ pub async fn on_new_block(
             .get(&time_frame)
             .unwrap()
             .yearly_rate_from_time_frame(time_frame);
-        let issuance_rate_yearly =
-            beacon_chain::estimated_issuance_from_time_frame(db_pool, &time_frame, block).await;
-        let supply_growth_rate = {
+        let (issuance_rate_yearly, average_eth_price) = join!(
+            beacon_chain::estimated_issuance_from_time_frame(db_pool, &time_frame, block),
+            usd_price::average_from_time_range(
+                db_pool,
+                time_frame.start_timestamp(block),
+                block.timestamp,
+            )
+        );
+        let issuance_rate_yearly_pow = EthUsdAmount {
+            eth: EthNewtype(PROOF_OF_WORK_YEARLY_ISSUANCE_ESTIMATE),
+            usd: UsdNewtype(PROOF_OF_WORK_YEARLY_ISSUANCE_ESTIMATE * average_eth_price.0),
+        };
+        let supply_growth_rate_yearly = {
             let eth_burn = burn_rate_yearly.eth;
             let eth_issuance = issuance_rate_yearly.eth;
+            let yearly_delta = eth_issuance - eth_burn;
+            yearly_delta.0 / eth_supply.0
+        };
+        let supply_growth_rate_yearly_pow = {
+            let eth_burn = burn_rate_yearly.eth;
+            let eth_issuance = issuance_rate_yearly_pow.eth;
             let yearly_delta = eth_issuance - eth_burn;
             yearly_delta.0 / eth_supply.0
         };
@@ -57,9 +75,9 @@ pub async fn on_new_block(
             GaugeRatesTimeFrame {
                 burn_rate_yearly,
                 issuance_rate_yearly,
-                issuance_rate_yearly_pow: issuance_rate_yearly,
-                supply_growth_rate_yearly: supply_growth_rate,
-                supply_growth_rate_yearly_pow: supply_growth_rate,
+                issuance_rate_yearly_pow,
+                supply_growth_rate_yearly,
+                supply_growth_rate_yearly_pow,
             },
         );
     }
