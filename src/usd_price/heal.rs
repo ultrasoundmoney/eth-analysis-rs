57c5a7a8973b18e4532015697927043cd8fb31e5
ckoopmann <c.k.e.koopmann@gmail.com>
2023-03-16 13:44:44 +0800

    Speed up heal-eth-prices by making requests concurrent

--- src/usd_price/heal.rs
+++ src/usd_price/heal.rs
@@ -7,6 +7,7 @@ use tracing::{debug, info};
 use crate::{db, execution_chain::LONDON_HARD_FORK_TIMESTAMP, log};
 
 use super::{bybit, store, EthPriceTimestamp};
+use futures::stream::{self, StreamExt};
 
 pub async fn heal_eth_prices() {
     log::init_with_env();
@@ -56,30 +57,49 @@ pub async fn heal_eth_prices() {
         .unwrap()
         .timestamp();
 
-    for minute_n in 0..minutes_since_london {
-        let timestamp = london_minute_timestamp + minute_n * 60;
-        if !known_minutes.contains(&timestamp) {
-            let timestamp_date_time = Utc.timestamp_opt(timestamp, 0).unwrap();
-            debug!(minute = timestamp_date_time.to_string(), "missing minute");
-            let usd = bybit::get_closest_price_by_minute(
-                timestamp_date_time,
-                Duration::minutes(max_distance_in_minutes),
-            )
-            .await;
-            match usd {
-                None => {
-                    debug!(
-                        timestamp = timestamp_date_time.to_string(),
-                        "no Bybit price available",
-                    );
-                }
-                Some(usd) => {
-                    debug!("found a price on Bybit, adding it to the DB");
-                    store::store_price(&mut connection, timestamp_date_time, usd).await;
-                }
+    let missing_minutes_timestamps = (0..minutes_since_london)
+        .map(|minutes| london_minute_timestamp + minutes * 60)
+        .filter(|timestamp| !known_minutes.contains(timestamp))
+        .collect::<Vec<i64>>();
+
+    let concurrent_requests = 50;
+    debug!("found {} missing minutes", missing_minutes_timestamps.len());
+    let mut missing_minutes_stream = stream::iter(missing_minutes_timestamps)
+        .map(|timestamp| {
+            async move {
+                let timestamp_date_time = Utc.timestamp_opt(timestamp, 0).unwrap();
+                debug!(minute = timestamp_date_time.to_string(), "missing minute");
+                let usd = bybit::get_closest_price_by_minute(
+                    timestamp_date_time,
+                    Duration::minutes(max_distance_in_minutes),
+                )
+                .await;
+                match usd {
+                    None => {
+                        debug!(
+                            timestamp = timestamp_date_time.to_string(),
+                            "no Bybit price available",
+                        );
+                    }
+                    Some(usd) => {
+                        debug!(
+                            "found a price on Bybit for timestamp: {} - {}",
+                            timestamp, usd
+                        );
+                    }
+                };
+                (usd, timestamp_date_time)
             }
+        })
+        .buffer_unordered(concurrent_requests);
+
+    while let Some((usd, timestamp))  = missing_minutes_stream.next().await {
+        if let Some(usd) = usd {
+            debug!("Storing price for timestamp: {:?}", timestamp);
+            store::store_price(&mut connection, timestamp, usd).await;
+            debug!("Stored price for timestamp: {:?}", timestamp);
         }
-    }
+    };
 
     info!("done healing eth prices");
 }
