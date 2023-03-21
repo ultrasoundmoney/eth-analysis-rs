3f7afa0748194700dbbe9a04b321a4980852915b
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-03-21 17:06:15 +0100

    fix(eth_supply): timestamp missing timezone

--- src/execution_chain/mod.rs
+++ src/execution_chain/mod.rs
@@ -77,5 +77,5 @@ lazy_static! {
     pub static ref LONDON_HARD_FORK_TIMESTAMP: DateTime<Utc> =
         "2021-08-05T12:33:42Z".parse::<DateTime<Utc>>().unwrap();
     pub static ref PARIS_HARD_FORK_TIMESTAMP: DateTime<Utc> =
-        "2022-09-15T06:42:59".parse::<DateTime<Utc>>().unwrap();
+        "2022-09-15T06:42:59Z".parse::<DateTime<Utc>>().unwrap();
 }
