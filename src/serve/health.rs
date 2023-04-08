fb8af204a20411d357a4ca7ef4f2dc81edc9d89c
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-04-08 10:50:56 +0200

    refactor(health): use `use`

--- src/serve/health.rs
+++ src/serve/health.rs
@@ -5,8 +5,8 @@ use chrono::{DateTime, Duration, Utc};
 use crate::health::{HealthCheckable, HealthStatus};
 
 pub struct ServeHealth {
-    last_cache_update: RwLock<Option<chrono::DateTime<chrono::Utc>>>,
-    started_on: chrono::DateTime<chrono::Utc>,
+    last_cache_update: RwLock<Option<DateTime<Utc>>>,
+    started_on: DateTime<Utc>,
 }
 
 impl ServeHealth {
@@ -18,7 +18,7 @@ impl ServeHealth {
     }
 
     pub fn set_cache_updated(&self) {
-        *self.last_cache_update.write().unwrap() = Some(chrono::Utc::now());
+        *self.last_cache_update.write().unwrap() = Some(Utc::now());
     }
 }
 
@@ -26,7 +26,7 @@ impl HealthCheckable for ServeHealth {
     // Cache is healthy if we have seen an update in the last five minutes, or it has been less
     // than 5 minutes since the server started.
     fn health_status(&self) -> HealthStatus {
-        let now = chrono::Utc::now();
+        let now = Utc::now();
         let last_update = self
             .last_cache_update
             .read()
