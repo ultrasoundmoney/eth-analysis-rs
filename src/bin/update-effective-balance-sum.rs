c32c36aa0cc8bec0dcd24b74468996fc972629aa
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-06-23 17:53:02 +0200

    fix(effective_balance_sums): import

--- src/bin/update-effective-balance-sum.rs
+++ src/bin/update-effective-balance-sum.rs
@@ -4,7 +4,7 @@ use eth_analysis::{
     beacon_chain::{self, BeaconNodeHttp},
     caching::{self, CacheKey},
     db,
-    effective_balance_sum::{self, EffectiveBalanceSum},
+    effective_balance_sums::{self, EffectiveBalanceSum},
     log,
 };
 
@@ -24,10 +24,10 @@ pub async fn main() {
         .expect("expect at least one beacon slot to be synced before updating effective balances");
 
     let sum =
-        effective_balance_sum::get_effective_balance_sum(&beacon_node, &last_state.state_root)
+        effective_balance_sums::get_effective_balance_sum(&beacon_node, &last_state.state_root)
             .await;
 
-    effective_balance_sum::store_effective_balance_sum(&db_pool, &last_state.state_root, &sum)
+    effective_balance_sums::store_effective_balance_sum(&db_pool, &last_state.state_root, &sum)
         .await;
 
     let effective_balance_sum = EffectiveBalanceSum::new(&last_state.slot, sum);
