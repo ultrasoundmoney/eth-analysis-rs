325af85d954c0b2e32078fd475e0a4fdfed7ce8a
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-11-09 19:54:13 +0100

    refactor(log): reuse builder
    Swaps PRETTY_PRINT with LOG_JSON flag.

--- src/log.rs
+++ src/log.rs
@@ -1,31 +1,19 @@
-use lazy_static::lazy_static;
-use tracing_subscriber::{
-    fmt::{self, Layer},
-    prelude::__tracing_subscriber_SubscriberExt,
-    util::SubscriberInitExt,
-    EnvFilter,
-};
+use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
 
-use crate::env::{self, Env};
-
-lazy_static! {
-    static ref PRETTY_PRINT: bool = env::get_env_bool("PRETTY_PRINT");
-}
+use crate::env::ENV_CONFIG;
 
 pub fn init_with_env() {
-    if *PRETTY_PRINT {
-        fmt::init();
+    let builder = tracing_subscriber::fmt().with_env_filter(EnvFilter::from_default_env());
+
+    let builder = if ENV_CONFIG.log_perf {
+        builder.with_span_events(FmtSpan::CLOSE)
     } else {
-        match *env::ENV {
-            Env::Dev => fmt::init(),
-            Env::Prod => tracing_subscriber::registry()
-                .with(Layer::default().json())
-                .with(EnvFilter::from_default_env())
-                .init(),
-            Env::Stag => tracing_subscriber::registry()
-                .with(Layer::default().json())
-                .with(EnvFilter::from_default_env())
-                .init(),
-        }
-    }
+        builder
+    };
+
+    if ENV_CONFIG.log_json {
+        builder.json().init();
+    } else {
+        builder.init();
+    };
 }
