7fdcc5af3e4f83824f78c26646e75f33d7240e19
christn <c.k.e.koopmann@gmail.com>
2024-03-11 16:26:37 +0100

    Adjust migrations

--- migrations/20240310183918_blocks_next_blob_fee.down.sql
+++ migrations/20240310183918_blocks_next_blob_fee.down.sql
@@ -1,6 +1,4 @@
 -- Add down migration script here
 ALTER TABLE blocks_next
     DROP COLUMN blob_gas_used,
-    DROP COLUMN blob_base_fee,
-    DROP COLUMN blob_fee_sum,
     DROP COLUMN excess_blob_gas;
--- migrations/20240310183918_blocks_next_blob_fee.up.sql
+++ migrations/20240310183918_blocks_next_blob_fee.up.sql
@@ -1,6 +1,4 @@
 -- Add up migration script here
 ALTER TABLE blocks_next
-    ADD COLUMN blob_gas_used bigint,
-    ADD COLUMN blob_base_fee bigint,
-    ADD COLUMN blob_fee_sum bigint,
-    ADD COLUMN excess_blob_gas bigint;
+    ADD COLUMN blob_gas_used INT4,
+    ADD COLUMN excess_blob_gas INT4;
